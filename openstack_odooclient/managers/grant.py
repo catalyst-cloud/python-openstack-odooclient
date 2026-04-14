a7fc73e4435fad3d80eedf61acb454291ea22f31
Callum Dickinson <callum.dickinson@catalystcloud.nz>
2026-04-15 08:47:39 +1200

    Add missing notes for expiry_date changes

--- openstack_odooclient/managers/grant.py
+++ openstack_odooclient/managers/grant.py
@@ -25,7 +25,11 @@ from ..base.record_manager.base import RecordManagerBase
 
 class Grant(RecordBase["GrantManager"]):
     expiry_date: date | Literal[False]
-    """The date the grant expires, if an expiry date is set."""
+    """The date the grant expires, if an expiry date is set.
+
+    *Changed in version 0.2.6*: Fixed incorrect annotation for ``expiry_date``
+    by marking it as optional.
+    """
 
     grant_type_id: Annotated[int, ModelRef("grant_type", GrantType)]
     """The ID of the type of this grant."""
